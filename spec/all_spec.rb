bb86e2dd571f0d4cd231ac786a24bf9029e16884
Reinkaos <rj.matthews23@gmail.com>
2016-07-21 20:48:52 +1200

    refactor

--- spec/all_spec.rb
+++ spec/all_spec.rb
@@ -6,29 +6,40 @@
 # are false or nil.
 
 RSpec.describe 'all?' do
-  it 'returns true when all of the words are greater than 3' do
-    example = %w(ant bear cat).all? { |w| w.length >= 3 }
-    expect(example).to eq true
+  context 'returns true' do
+    after do
+      expect(@example).to eq true
+    end
+
+    it 'returns true when all of the words are greater than 3' do
+      @example = %w(ant bear cat).all? { |w| w.length >= 3 }
+    end
+
+    it 'returns true when all of the values are not false or nil' do
+      @example = [100, 500, 1].all?
+    end
+
+    it 'returns true because all of the elements in the array are integers' do
+      @example = [100, 200, 1].all? { |i| i.is_a? Integer }
+    end
   end
 
-  it 'returns false when all of the words are not greater than 5' do
-    example = %w(ant bear cat dog).all? { |w| w.length >= 5 }
-    expect(example).to eq false
-  end
+  context 'returns false' do
+    after do
+      expect(@example).to eq false
+    end
 
-  # This is the implicit block of { |obj| obj }
-  it 'returns false when there is a nil value provided' do
-    example = [nil, true, 99].all?
-    expect(example).to eq false
-  end
+    it 'returns false when all of the words are not greater than 5' do
+      @example = %w(ant bear cat dog).all? { |w| w.length >= 5 }
+    end
 
-  it 'returns true when all of the values are not false or nil' do
-    example = [100, 500, 1].all?
-    expect(example).to eq true
-  end
+    # This is the implicit block of { |obj| obj }
+    it 'returns false when there is a nil value provided' do
+      @example = [nil, true, 99].all?
+    end
 
-  it 'returns true because all of the elements in the array are integers' do
-    example = [100, 500, 1].all? { |w| w.is_a? Integer }
-    expect(example).to eq true
+    it 'returns false when you expect all to be an int, but one is a string' do
+      @example = [1, 2, 'three'].all? { |i| i.is_a? Integer }
+    end
   end
 end
